9a643c29e8b634035e1ccac202428bc2bb83da5b
Guntur <gunturpdng@gmail.com>
2023-12-31 21:39:30 +0800

    grading exercise2.py

--- file/exercise2.py
+++ file/exercise2.py
@@ -1,10 +1,15 @@
-fname = 'mbox-short.txt'
-fh = open(fname)
-text = "X-DSPAM-Confidence:"
-for line in fh:
-    if not line.startswith(text):
-        continue
-    print(line[20:])
-    
+def XDSPAM():
+    fname = input("Input File Name : ") # mbox-short.txt
+    fh = open(fname)
+    text = "X-DSPAM-Confidence:"
+    som=0
+    count=0
+    for line in fh:
+        if not line.startswith(text):
+            continue
+        floatline = float(line[20:])
+        som += floatline
+        count += 1
+    print("Average spam confidence:", som/count)
 
-print("Done")
+XDSPAM()
